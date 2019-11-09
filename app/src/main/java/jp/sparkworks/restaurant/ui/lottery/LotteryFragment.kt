e68bd7a4c19d5bde4af7e62766da8d1d906d1200
Xiaolin <tousyourin@yahoo.co.jp>
2019-11-09 17:06:35 +0900

    プロジェクト作成

---
+++ app/src/main/java/jp/sparkworks/restaurant/ui/lottery/LotteryFragment.kt
@@ -0,0 +1,31 @@
+package jp.sparkworks.restaurant.ui.lottery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import jp.sparkworks.restaurant.R
+
+class LotteryFragment : Fragment() {
+
+    private lateinit var lotteryViewModel: LotteryViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        lotteryViewModel =
+            ViewModelProviders.of(this).get(LotteryViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_lottery, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        lotteryViewModel.text.observe(this, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
---
+++ app/src/main/java/jp/sparkworks/restaurant/ui/lottery/LotteryViewModel.kt
@@ -0,0 +1,13 @@
+package jp.sparkworks.restaurant.ui.lottery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LotteryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
